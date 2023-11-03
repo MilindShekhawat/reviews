9047cd2f28e0cdbee90886b7e090cf3b3d7d2a63
MilindShekhawat <milindshekhawat10@gmail.com>
2023-11-03 12:01:10 +0530

    Added images

--- app/components/Games.js
+++ app/components/Games.js
@@ -1,7 +1,14 @@
 'use client'
 import { useRouter } from "next/navigation"
 import Image from "next/image";
-import brief from '../img/briefcase.png'
+import path from '../img/path of exile.jpg'
+import destiny from '../img/destiny 2 lightfall.jpg'
+import baldur from '../img/baldur\'s gate 3.jpg'
+import evil from '../img/resident evil 4.jpg'
+import elden from '../img/elden ring.jpg'
+import rubicon from '../img/armored core 6.jpg'
+import diablo from '../img/diablo 4.jpg'
+import starfield from '../img/starfield.jpg'
 import AddGame from "./AddGame";
 
 function GameList(props) {
@@ -12,7 +19,11 @@ function GameList(props) {
     console.log(slug);
     router.push(`../${id}/${slug}`)
   }
-
+  function imageSelector(id) {
+    const images = [,path,destiny,baldur,evil,elden,rubicon,diablo,starfield]
+    return images[id]
+  }
+  
   return (
     <div className='flex flex-col'>
       <div className="flex items-center">
@@ -22,8 +33,8 @@ function GameList(props) {
       <ul className='flex flex-wrap gap-3 my-5'>
         {props.games.map((game) => (
           <li key={game.id} onClick={() => handleClick(game.id, game.title)} 
-          className='relative w-[256px] aspect-card'>
-            <Image src=''width={2000} height={3000} className="object-cover" alt=""/>
+          className='relative w-[256px] aspect-card rounded-sm'>
+            <Image src={imageSelector(game.id)} width={2000} height={3000} className="object-contain rounded-sm" alt="game cover"/>
             <div className='p-5 absolute bottom-0 left-0 right-0 bg-gradient-to-t via-black from-black rounded-sm'>
               <ul className='flex flex-wrap gap-x-1 h-[40px] text-sm content-end'>
                 {
